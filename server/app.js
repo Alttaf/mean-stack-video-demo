cbc83a6ead2ec18ac4c66016272500b1cddcdfff
alttaf hussain <alttaf.hussain@gmail.com>
2014-09-15 00:04:42 +0100

    initial commit

---
+++ server/app.js
@@ -0,0 +1,33 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('express');
+var config = require('./config/environment');
+// Setup server
+var app = express();
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes')(app);
+// setup Mongo
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/sunblock');
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
